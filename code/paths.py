244902e5abd2777779a3fb515e0b4cc01a1534bb
arbuz001 <alexey.arbuzov@gmail.com>
2018-01-27 20:59:38 +0100

    added location of paths to look for python modules to include

--- code/paths.py
+++ code/paths.py
@@ -2,7 +2,22 @@
 Contains paths to data and submission folders
 '''
 import os
+import sys
+
+from pprint import pprint
 
 # all project foldres consolidated
+
+# 0. print all current sys.paths (i.e. locations where python looks for modules)
+pprint(sys.path)
+
 strPrjPath = 'c:/works-and-documents/svn/kaggle/data-fx-historical'
-strDataPath = os.path.join(strPrjPath, '/data')
+
+strDataPath = strPrjPath + '/data/'
+strDataCode = strPrjPath + '/code/'
+
+sys.path.insert(0, strDataPath)
+sys.path.insert(0, strDataCode)
+
+# 1. two new sys.paths to look for modules should be added 
+pprint(sys.path)
