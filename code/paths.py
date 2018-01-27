12feec3662ec8b42c0e9f1dbf424ebc6103d7cae
arbuz001 <alexey.arbuzov@gmail.com>
2018-01-27 22:56:01 +0100

    removing .pyc files

--- code/paths.py
+++ code/paths.py
@@ -1,23 +1,28 @@
 '''
 Contains paths to data and submission folders
 '''
+
+strPrjPath = '/home/alex/Documents/kaggle/data-fx-historical'
+
 import os
 import sys
+import rlcompleter
+import readline
 
 from pprint import pprint
 
-# all project foldres consolidated
+# 0. add autocomplete by tab feature
+readline.parse_and_bind("tab: complete")
 
-# 0. print all current sys.paths (i.e. locations where python looks for modules)
+# 1. print all current sys.paths (i.e. locations where python looks for modules)
 pprint(sys.path)
 
-strPrjPath = 'c:/works-and-documents/svn/kaggle/data-fx-historical'
-
-strDataPath = strPrjPath + '/data/'
-strDataCode = strPrjPath + '/code/'
+# all project foldres consolidated
+strDataPath = strPrjPath + '/data'
+strDataCode = strPrjPath + '/code'
 
 sys.path.insert(0, strDataPath)
 sys.path.insert(0, strDataCode)
 
-# 1. two new sys.paths to look for modules should be added 
+# 2. two new sys.paths to look for modules should be added 
 pprint(sys.path)
